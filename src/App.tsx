557ce3371e19a9c8a04a868c702bf67202975868
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-24 17:48:00 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { FC } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme'; // Importar el tema
+
 import Home from './pages/Home';
 import Details from './pages/Details';
 import Contact from './pages/Contact';
 import Project from './pages/Project';
-import Services from './pages/Services'; // Importar la página de Servicios
+import Services from './pages/Services';
 import Navbar from './components/Navbar';
 import Logo from './components/Logo';
-import { cards } from './assets/cardsData'; // Importa las tarjetas desde cardsData
-import theme from './theme';
+import { cards } from './assets/cardsData'; // Importar las tarjetas
 
 const App: FC = () => {
   return (
@@ -20,8 +22,8 @@ const App: FC = () => {
           <Navbar />
         </header>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<Home cards={cards} />} /> {/* Corregir: Pasar `cards` como `prop` */}
+          <Route path="/details/:id" element={<Details cards={cards} />} /> {/* Corregir: Pasar `cards` como `prop` */}
           <Route path="/contact" element={<Contact />} />
           <Route path="/project" element={<Project />} />
           <Route path="/services" element={<Services />} />
