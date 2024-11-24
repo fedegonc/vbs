607f9b370778317220d78d02a04f21a9c86a5543
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-24 15:08:22 -0300

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Puedes definir estilos para tu barra de navegación
+
+const Navbar: FC = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <Link to="/" className="navbar-link">Home</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/project" className="navbar-link">Proyecto</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/contact" className="navbar-link">Contacto</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
