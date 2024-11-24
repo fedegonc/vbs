02a6ec323b3ccb9632917d31830cee676ea8c4b9
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-24 15:02:41 -0300

    Create Project.tsx

---
+++ src/pages/Project.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const Project: FC = () => {
+  return (
+    <main>
+      <h1>Proyecto</h1>
+      <p>Bienvenido a la página de nuestro proyecto. Aquí puedes encontrar información detallada sobre los objetivos y la visión del proyecto.</p>
+    </main>
+  );
+};
+
+export default Project;
