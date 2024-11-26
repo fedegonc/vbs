658ace4c973802bad75ae015fc7bf71017b20112
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-25 21:01:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { FC } from 'react';
 import Home from './pages/Home';
 import Details from './pages/Details';
