d9445447261b0422a098ad3716aa12dac50e1fda
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-26 13:02:18 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Details from './pages/Details';
 import Contact from './pages/Contact';
 import Project from './pages/Project';
+import Gallery from './pages/Gallery';
 import Services from './pages/Services';
 import Navbar from './components/Navbar';
 import { cards } from './assets/cardsData';
@@ -16,8 +17,8 @@ const App: FC = () => {
         <Navbar />
       </header>
       <Routes>
-        
-        <Route path="/" element={<Home cards={cards} />} /> {/* Aquí se pasa `cards` como prop */}
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Gallery cards={cards} />} /> {/* Aquí se pasa `cards` como prop */}
         <Route path="/details/:id" element={<Details cards={cards} />} /> {/* Aquí también */}
         <Route path="/contact" element={<Contact />} />
         <Route path="/project" element={<Project />} />
