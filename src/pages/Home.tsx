1249426466a6e4519f00e2f20b9cf101cdfbb3fc
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-26 11:03:00 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import PageLayout from './PageLayout';
 
 interface Card {
   id: number;
@@ -16,9 +17,9 @@ const Home: FC<HomeProps> = ({ cards }) => {
   const navigate = useNavigate();
 
   return (
-    <main>
+    <PageLayout>
       <h1>Galería de Arte</h1>
-      <div className="button-container">
+      <div className="button-container mb-4">
         <button onClick={() => navigate('/contact')} className="navigation-button">
           Contáctanos
         </button>
@@ -26,19 +27,24 @@ const Home: FC<HomeProps> = ({ cards }) => {
           Proyecto
         </button>
       </div>
-      <div className="card-container">
+      <div className="card-container d-flex flex-wrap justify-content-center">
         {cards.map((card: Card) => (
-          <div className="card" key={card.id}>
-            <img src={card.image} alt={card.title} className="card-image" />
-            <h2 className="card-title">{card.title}</h2>
-            <p className="card-description">{card.description}</p>
-            <button className="card-button" onClick={() => navigate(`/details/${card.id}`)}>
-              Ver Más Detalles
-            </button>
+          <div className="card m-3" key={card.id} style={{ width: '18rem' }}>
+            <img src={card.image} alt={card.title} className="card-img-top" />
+            <div className="card-body">
+              <h5 className="card-title">{card.title}</h5>
+              <p className="card-text">{card.description}</p>
+              <button
+                className="btn btn-primary"
+                onClick={() => navigate(`/details/${card.id}`)}
+              >
+                Ver Más Detalles
+              </button>
+            </div>
           </div>
         ))}
       </div>
-    </main>
+    </PageLayout>
   );
 };
 
